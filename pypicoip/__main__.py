4ea4d1c3003ba3f0304e161fb1923476eb5b5376
Orlin Dimitrov <orlin369@gmail.com>
2024-03-11 23:52:41 +0200

    Add main executable file

--- pypicoip/__main__.py
+++ pypicoip/__main__.py
@@ -27,12 +27,18 @@ SOFTWARE.
 
 """
 
+import sys
+import argparse
+import traceback
+
+from pypicoip.http.client import PicoIP
+
 #region File Attributes
 
 __author__ = "Orlin Dimitrov"
 """Author of the file."""
 
-__copyright__ = "Copyright 2020, Orlin Dimitrov"
+__copyright__ = "Copyright 2024, Orlin Dimitrov"
 """Copyright holder"""
 
 __credits__ = []
@@ -53,10 +59,47 @@ __status__ = "Debug"
 
 #endregion
 
-from .http.client import PicoIP as client
-
 def main():
-    cl = client(host="127.0.0.1")
 
-if __name__ == '__main__':
-    main()
+    # Create parser.
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--host", type=str, default="172.16.100.2", help="Host/IP of the PicoIP.")
+    parser.add_argument("--user", type=str, default="admin", help="Username of the PicoIP.")
+    parser.add_argument("--password", type=str, default="admin", help="Password of the PicoIP.")
+    parser.add_argument("--p3", type=int, default=0, help="Port 3 of the PicoIP.")
+    parser.add_argument("--p5", type=int, default=0, help="Port 5 of the PicoIP.")
+
+    # Take arguments.
+    args = parser.parse_args()
+
+    with PicoIP(host=args.host, user=args.user, password=args.password) as client:
+
+        if args.p3 is not None:
+            client.set_p3(args.p3)
+
+        if args.p5 is not None:
+            client.set_p5(args.p5)
+
+        result = []
+        result.append(client.get_p3())
+        result.append(client.get_p5())
+        result.append(client.get_p6())
+        result += client.get_adc()
+
+        print(result)
+
+        # state = client.digitalRead(0)
+        # print(state)
+        # state = client.analogRead(0)
+        # print(state)
+        # client.digitalWrite(1, 1)
+        # print("OK")
+        # client.digitalWrite(0, 0)
+        # print("OK")
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main())
+    except Exception as e:
+        print(traceback.format_exc())
